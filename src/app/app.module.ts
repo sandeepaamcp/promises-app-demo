14ebb79fecf29bc1e19336ca2a60b762892e7de3
sandeepa amcp <sandeepaamcp@gmail.com>
2019-02-17 15:13:48 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { PostsComponent } from './components/posts/posts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent,
   ],
   imports: [
     BrowserModule
